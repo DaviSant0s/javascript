b725c5ce73a2d207a70b6188ffb56cf242e49b5e
Davi Santos <daviir17@gmail.com>
2023-12-25 22:04:38 -0300

    feat(parte 1): aula

---
+++ secao6-objetos-e-prototypes/aula98-Factory_Functions+Prototypes/index.js
@@ -0,0 +1,21 @@
+ function CriaPessoa(nome, sobrenome){
+    const pessoaPrototype = {
+        falar() {
+            console.log(`${this.nome} está falando.`);
+        },
+        comer() {
+            console.log(`${this.nome} está comendo.`);
+        },
+        beber() {
+            console.log(`${this.nome} está bebendo.`);
+        },
+    }
+
+    return Object.create(pessoaPrototype, {
+        nome: {value: nome},
+        sobrenome: {value: sobrenome},
+    });
+ }
+
+ const p1 = CriaPessoa('luiz', 'Otávio');
+ console.log(p1);
