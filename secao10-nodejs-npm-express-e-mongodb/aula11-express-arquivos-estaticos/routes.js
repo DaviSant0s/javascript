92acd1a4aeceda1cca9b4f0316f2022e7cdb81f4
Davi Santos <daviir17@gmail.com>
2024-01-25 15:47:52 -0300

    feat(aula 11): express Arquivos estáticos

---
+++ secao10-nodejs-npm-express-e-mongodb/aula11-express-arquivos-estaticos/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController')
+
+// Rotas home
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+
+
+// para ser possível exportar
+module.exports = route;
