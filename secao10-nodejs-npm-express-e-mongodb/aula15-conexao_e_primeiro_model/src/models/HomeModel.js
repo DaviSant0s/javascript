74db901d8053c4189d352d38599162f24c731a76
Davi Santos <daviir17@gmail.com>
2024-01-27 18:50:26 -0300

    feat(aula15): conexão e Primeiro Model

---
+++ secao10-nodejs-npm-express-e-mongodb/aula15-conexao_e_primeiro_model/src/models/HomeModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// Schema é a modelagem dos nossos dados
+const HomeSchema = new mongoose.Schema({
+    titulo: {type: String, required: true},
+    descricao: String
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+module.exports = HomeModel;
